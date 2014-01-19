b22c9806d69bd6ad7930dc4ef25ac6195d6ec256
Andre Weissflog <floooh@gmail.com>
2014-01-19 15:48:47 +0100

    Started with ArrayMap container class

---
+++ code/Core/Containers/ArrayMap.h
@@ -0,0 +1,181 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+    @class Oryol::Core::ArrayMap
+    
+    An ArrayMap works like a Map, but stores the values in a separate
+    array from keys. This is useful if:
+    
+    - the value type is big and/or has an expensive copy/move operation
+    - the values must remain in the order they have been added
+    - erasing is only fast when the order of values is not important, 
+      in this case, use EraseSwap (see notes on erasing below)
+    
+    Iterating over an ArrayMap with begin(), end() will iterate over 
+    the values in the order the values have been added.
+    
+    The operator[] will access elements by key (just as in a map). To 
+    access an element by its value index, use ValueAtIndex()!
+    
+    Be careful about erasing elements, this can be slow:
+    - EraseSwap() is best, this do a swap in the value array, and a move
+      in the key map
+    - Erase() and EraseIndex() need to do a sweep over the key map to
+      fix-up indices, and are thus O(N)!!!
+*/
+#include "Core/Config.h"
+#include "Core/Containers/Array.h"
+#include "Core/Containers/Dictionary.h"
+
+namespace Oryol {
+namespace Core {
+
+template<class KEY, class VALUE> class ArrayMap {
+public:
+    /// default constructor
+    ArrayMap() {
+    };
+    /// construct with allocation strategy
+    ArrayMap(int32 minGrow, int32 maxGrow=ORYOL_CONTAINER_DEFAULT_MAX_GROW) :
+        indexMap(minGrow, maxGrow),
+        values(minGrow, maxGrow) {
+    };
+    /// copy constructor (truncates to actual size)
+    ArrayMap(const ArrayMap& rhs) :
+        indexMap(rhs.indexMap),
+        valueArray(rhs.valueArray) {
+    };
+    /// move constructor (same capacity and size)
+    ArrayMap(ArrayMap&& rhs) :
+        indexMap(std::move(rhs.indexMap)),
+        valueArray(std::move(rhs.valueArray)) {
+    };
+    /// copy-assignment operator (truncates to actual size)
+    void operator=(const ArrayMap& rhs) {
+        this->indexMap = rhs.indexMap;
+        this->valueArray = rhs.valueMap;
+    };
+    /// move-assignment operator (same capacity and size)
+    void operator=(ArrayMap&& rhs) {
+        this->indexMap = std::move(this->indexMap);
+        this->valueArray = std::move(this->valueArray);
+    };
+
+    /// set allocation strategy
+    void SetAllocStrategy(int32 minGrow, int32 maxGrow=ORYOL_CONTAINER_DEFAULT_MAX_GROW) {
+        this->indexMap.SetAllocStrategy(minGrow, maxGrow);
+        this->valueArray.SetAllocStrategy(minGrow, maxGrow);
+    };
+    /// get min grow value
+    int32 GetMinGrow() const {
+        return this->valueArray.GetMinGrow();
+    };
+    /// get max grow value
+    int32 GetMaxGrow() const {
+        return this->valueArray.GetMaxGrow();
+    };
+    /// get number of elements in array
+    int32 Size() const {
+        return this->valueArray.Size();
+    };
+    /// return true if empty
+    bool Empty() const {
+        return this->valueArray.Empty();
+    };
+    /// get capacity of array
+    int32 Capacity() const {
+        return this->valueArray.Capacity();
+    };
+    
+    /// test if an element exists
+    bool Contains(const KEY& key) const {
+        return this->indexMap.Contains(key);
+    };
+
+    /// read/write access single element by key
+    VALUE& operator[](const KEY& key) {
+        return this->valueArray[this->indexMap[key]];
+    };
+    /// read-only access single element by key
+    const VALUE& operator[](const KEY& key) const {
+        return this->valueArray[this->indexMap[key]];
+    };
+    
+    /// increase capacity to hold at least numElements more elements
+    void Reserve(int32 numElements) {
+        this->indexMap.Reserve(numElements);
+        this->valueArray.Reserve(numElements);
+    };
+    /// trim capacity to size (this involves a re-alloc)
+    void Trim() {
+        this->indexMap.Trim();
+        this->valueArray.Trim();
+    };
+    /// clear the array (deletes elements, keeps capacity)
+    void Clear() {
+        this->indexMap.Clear();
+        this->valueArray.Clear();
+    };
+    
+    /// insert-copy new element
+    void Insert(const KEY& key, const VALUE& value) {
+        this->valueArray.AddBack(value);
+        this->indexMap.Insert(key, this->valueArray.Size() - 1);
+    };
+    /// insert-move element
+    void Insert(KEY&& key, VALUE&& value) {
+        this->valueArray.AddBack(std::move(value));
+        this->indexMap.Insert(std::move(key), this->valueArray.Size() - 1);
+    };
+    
+    /// find value index
+    int32 FindValueIndex(const KEY& key) {
+        return this->indexMap[key];
+    };
+    /// read-access value at index
+    const VALUE& ValueAtIndex(int32 valueIndex) const {
+        return this->valueArray[valueIndex];
+    };
+    /// read-write-access value at index
+    VALUE& ValueAtIndex(int32 valueIndex) {
+        return this->valueArray[valueIndex];
+    };
+    
+    /// erase element by key, reasonably fast, but destroys value ordering
+    void EraseSwap(const KEY& key) {
+        o_error("FIXME!");
+    };
+    /// erase element by key, keep value order (involves moving values around)
+    void Erase(const KEY& key) {
+        o_error("FIXME!");
+    };
+    /// erase element by value index, most expensive erase operation
+    void EraseIndex(int32 valueIndex) {
+        o_error("FIXME!");
+    };
+    
+    /// C++ conform begin, MAY RETURN nullptr!
+    VALUE* begin() {
+        return this->valueArray.begin();
+    };
+    /// C++ conform begin, MAY RETURN nullptr!
+    const VALUE* begin() const {
+        return this->valueArray.begin();
+    };
+    /// C++ conform end,  MAY RETURN nullptr!
+    VALUE* end() {
+        return this->valueArray.end();
+    };
+    /// C++ conform end, MAY RETURN nullptr!
+    VALUE* end() const {
+        return this->valueArray.end();
+    };
+    
+private:
+    Dictionary<KEY, int32> indexMap;        // maps keys to indices into value array
+    Array<VALUE> valueArray;
+};
+    
+} // namespace Core
+} // namespace Oryol
+
