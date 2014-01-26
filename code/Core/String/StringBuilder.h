e2e9e0c3b676c1de310b7c872ab51f57ce1d8ee0
Andre Weissflog <floooh@gmail.com>
2014-01-26 12:36:31 +0100

    StringBuilder header

--- code/Core/String/StringBuilder.h
+++ code/Core/String/StringBuilder.h
@@ -6,7 +6,7 @@
     Helper class for dynamically building strings.
 */
 #include "Core/Types.h"
-#include "Core/String.h"
+#include "Core/String/String.h"
 
 namespace Oryol {
 namespace Core {
@@ -18,12 +18,18 @@ public:
     /// destructor
     ~StringBuilder();
     
-    /// reserve space
+    /// reserve space (numBytes excludes the terminating 0 byte)
     void Reserve(int32 numBytes);
     /// get capacity
     int32 Capacity() const;
     /// get length (in bytes)
     int32 Length() const;
+    /// clear the string builder
+    void Clear();
+    /// get the resulting string
+    String GetString() const;
+    /// get a substring, if endIndex is 0, until end-of-string
+    String GetSubString(int32 startIndex, int32 endIndex) const;
     
     /// append a single character
     void Append(char c);
@@ -33,8 +39,8 @@ public:
     void Append(const char* str, int32 length);
     /// append string
     void Append(const String& str);
-    /// append substring
-    void Append(const String& str, int32 index, int32 length);
+    /// append range from string, if endIndex is 0, copy until end of string
+    void Append(const String& str, int32 startIndex, int32 endIndex);
     /// append a list of strings
     void Append(std::initializer_list<String> list);
     /// append a list of strings with delimiter
@@ -43,7 +49,16 @@ public:
     /// truncate at index
     void Truncate(int32 index);
     /// remove the last char
-    void PopBack();
+    char PopBack();
+    
+private:
+    /// make sure that at least numBytes are available at end of string buffer 
+    void ensureRoom(int32 numBytes);
+    
+    static const int minGrowSize = 128;
+    char* buffer;
+    int32 capacity;
+    int32 size;
 };
     
 } // namespace Core
