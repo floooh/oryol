e9ef3d78841c1c7c0e5fee0e0bc7db1f5afe0f44
Andre Weissflog <floooh@gmail.com>
2014-02-15 15:26:02 +0100

    helper macros to simplify ToString and FromString testing

--- code/Modules/IO/UnitTests/OpenModeTest.cc
+++ code/Modules/IO/UnitTests/OpenModeTest.cc
@@ -12,18 +12,18 @@ using namespace Oryol::IO;
 
 TEST(OpenModeTest) {
     
-    CHECK(OpenMode::Invalid == 5);  // this will fail if we need to add more modes
+    CHECK(OpenMode::InvalidOpenMode == 5);  // this will fail if we need to add more modes
     CHECK(String(OpenMode::ToString(OpenMode::ReadOnly)) == "ReadOnly");
     CHECK(String(OpenMode::ToString(OpenMode::WriteOnly)) == "WriteOnly");
     CHECK(String(OpenMode::ToString(OpenMode::WriteAppend)) == "WriteAppend");
     CHECK(String(OpenMode::ToString(OpenMode::ReadWrite)) == "ReadWrite");
     CHECK(String(OpenMode::ToString(OpenMode::ReadWriteAppend)) == "ReadWriteAppend");
-    CHECK(String(OpenMode::ToString(OpenMode::Invalid)) == "Invalid");
+    CHECK(String(OpenMode::ToString(OpenMode::InvalidOpenMode)) == "InvalidOpenMode");
     
     CHECK(OpenMode::FromString("ReadOnly") == OpenMode::ReadOnly);
     CHECK(OpenMode::FromString("WriteOnly") == OpenMode::WriteOnly);
     CHECK(OpenMode::FromString("WriteAppend") == OpenMode::WriteAppend);
     CHECK(OpenMode::FromString("ReadWrite") == OpenMode::ReadWrite);
     CHECK(OpenMode::FromString("ReadWriteAppend") == OpenMode::ReadWriteAppend);
-    CHECK(OpenMode::FromString("Invalid") == OpenMode::Invalid);
+    CHECK(OpenMode::FromString("InvalidOpenMode") == OpenMode::InvalidOpenMode);
 }
