2c1220001414fc7fd4b92ebd6b3b6cddac4f22c4
Andre Weissflog <floooh@gmail.com>
2014-02-19 17:59:44 +0100

    Cleaned up windows precompiled.h

--- code/Modules/Core/windows/precompiled.h
+++ code/Modules/Core/windows/precompiled.h
@@ -5,20 +5,4 @@
     
     Standard includes for Windows platform.
 */
-#include <cstdarg>
-#include <cstdint>
-#include <cstdlib>
-#include <cstdio>
 #include <cstddef>
-#include <memory>
-#include <cstring>
-#include <cwchar>
-#include <typeinfo>
-#include <typeindex>
-#include <string>
-#include <type_traits>
-#include <algorithm>
-#include <atomic>
-#include <thread>
-#include <mutex>
-
