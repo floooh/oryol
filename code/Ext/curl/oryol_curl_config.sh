06e0066040251a63b22a070a7bd3978e96727d63
Andre Weissflog <floooh@gmail.com>
2014-02-20 16:27:04 +0100

    Added statically linked libcurl for Linux port

---
+++ code/Ext/curl/oryol_curl_config.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./configure --disable-shared --enable-http --disable-ftp --disable-file --disable-ldap --disable-rtsp --disable-dict --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smtp --disable-gopher --disable-manual --disable-sspi --disable-crypto-auth --disable-ntlm-wb --disable-tls-srp --disable-cookies --without-ssl --without-gnutls --without-polarssl --without-cyassl --without-nss --without-axtls --without-ca-path --without-libmetalink --without-libssh2 --without-librtmp --without-winidn --without-libidn --without-nghttp2
