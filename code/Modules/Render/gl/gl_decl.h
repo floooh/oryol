b422fa6a4ead81ae0b72bcc1268c619db5fa48ee
Andre Weissflog <floooh@gmail.com>
2014-03-16 17:46:24 +0100

    Some cleanup, and split gl header and full impl and minimal decl header

---
+++ code/Modules/Render/gl/gl_decl.h
@@ -0,0 +1,26 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+    Minimal GL declaration file. Include this in headers for fast compilation
+    and move actual GL code out of headers.
+*/
+
+#ifndef GL_VERSION_1_1
+typedef unsigned int GLenum;
+typedef unsigned int GLbitfield;
+typedef unsigned int GLuint;
+typedef int GLint;
+typedef int GLsizei;
+typedef unsigned char GLboolean;
+typedef signed char GLbyte;
+typedef short GLshort;
+typedef unsigned char GLubyte;
+typedef unsigned short GLushort;
+typedef unsigned long GLulong;
+typedef float GLfloat;
+typedef float GLclampf;
+typedef double GLdouble;
+typedef double GLclampd;
+typedef void GLvoid;
+#endif
+
