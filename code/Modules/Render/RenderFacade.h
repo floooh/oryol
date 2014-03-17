4334b5923c4a51f0313a867c12ff64d3db1fa9ae
Andre Weissflog <floooh@gmail.com>
2014-03-17 20:41:48 +0100

    RenderFacade interface prototype

--- code/Modules/Render/RenderFacade.h
+++ code/Modules/Render/RenderFacade.h
@@ -8,6 +8,14 @@
 */
 #include "Core/Macros.h"
 #include "Render/Core/displayMgr.h"
+#include "IO/Stream.h"
+#include "Resource/Id.h"
+#include "Resource/State.h"
+#include "Resource/Locator.h"
+#include "Render/Types/TransformType.h"
+#include "Render/Types/State.h"
+#include "Render/Core/PrimitiveGroup.h"
+#include "glm/fwd.hpp"
 
 namespace Oryol {
 namespace Render {
@@ -36,10 +44,64 @@ public:
     /// get the current actual display attributes (can be different from setup)
     const DisplayAttrs& GetDisplayAttrs() const;
     
+    /// attach a display event handler
+    void AttachEventHandler(const Core::Ptr<Messaging::Port>& handler);
+    /// detach a display event handler
+    void DetachEventHandler(const Core::Ptr<Messaging::Port>& handler);
+    
+    /// create a resource
+    template<class SETUP> Resource::Id CreateResource(const SETUP& setup);
+    /// create a resource with data stream
+    template<class SETUP> Resource::Id CreateResource(const SETUP& setup, const Core::Ptr<IO::Stream>& data);
+    /// discard a resource
+    void DiscardResource(const Resource::Id& resId) const;
+    /// lookup a resource by resource locator
+    Resource::Id LookupResource(const Resource::Locator& locator) const;
+    /// get the loading state of a resource
+    Resource::State::Code QueryResourceState(const Resource::Id& resId) const;
+    /// get one of the transform matrices
+    const glm::mat4& QueryTransform(TransformType::Code transformType) const;
+    
     /// begin frame rendering
     bool BeginFrame();
     /// end frame rendering
     void EndFrame();
+
+    /// apply the model transform
+    void ApplyModelTransform(const glm::mat4& model);
+    /// apply the view transform
+    void ApplyViewTransform(const glm::mat4& view);
+    /// apply the projection transform
+    void ApplyProjTransform(const glm::mat4& proj);
+    /// apply a render target to use
+    void ApplyRenderTarget(const Resource::Id& resId);
+    /// apply mesh to use for rendering
+    void ApplyMesh(const Resource::Id& resId);
+    /// apply program to use for rendering
+    void ApplyProgram(const Resource::Id& resId, uint32 selector=0);
+    /// apply a render state block
+    void ApplyStateBlock(const Resource::Id& resId);
+    /// apply a shader constant block
+    void ApplyConstantBlock(const Resource::Id& resId);
+    /// apply state with 1..4 arguments
+    template<typename... ARGS> void ApplyState(State::Code state, ARGS... args);
+    /// apply a shader variable
+    template<class T> void ApplyVariable(int32 index, const T& value);
+    /// apply a shader variable array
+    template<class T> void ApplyVariableArray(int32 index, const T* values, int32 numValues);
+    
+    /// clear the currently assigned render target
+    void Clear(bool color, bool depth, bool stencil);
+    /// submit a draw call with primitive group index in current mesh
+    void Draw(int32 primGroupIndex);
+    /// submit a draw call with overridden primitive group
+    void Draw(const PrimitiveGroup& primGroup);
+    /// draw multiple instances
+    void Draw(int32 primGroupIndex, const glm::mat4* instanceTransforms, int32 numInstances);
+    /// draw multiple instances with primitive group override
+    void Draw(const PrimitiveGroup& primGroup, const glm::mat4* instanceTransforms, int32 numInstances);
+    /// draw a fullscreen quad
+    void DrawFullscreenQuad();
 
 private:
     displayMgr displayManager;
