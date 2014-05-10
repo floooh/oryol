ebc4edf07ce5c06c5d63fa6f719b1e7ae47365d0
Andre Weissflog <floooh@gmail.com>
2014-05-10 14:49:50 +0200

    Added a draw call performance sample

---
+++ code/Samples/Render/DrawCallPerf/DrawCallPerf.cc
@@ -0,0 +1,207 @@
+//------------------------------------------------------------------------------
+//  DrawCallPerf.cc
+//------------------------------------------------------------------------------
+#include "Pre.h"
+#include "Core/App.h"
+#include "Render/RenderFacade.h"
+#include "Render/Util/RawMeshLoader.h"
+#include "Render/Util/ShapeBuilder.h"
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/random.hpp"
+#include <chrono>
+
+using namespace Oryol;
+using namespace Oryol::Core;
+using namespace Oryol::Render;
+using namespace Oryol::Resource;
+
+// derived application class
+class DrawCallPerfApp : public App {
+public:
+    virtual AppState::Code OnInit();
+    virtual AppState::Code OnRunning();
+    virtual AppState::Code OnCleanup();
+    
+private:
+    void updateCamera();
+    void emitParticles();
+    void updateParticles();
+
+    RenderFacade* render = nullptr;
+    Resource::Id meshId;
+    Resource::Id progId;
+    glm::mat4 view;
+    glm::mat4 proj;
+    glm::mat4 model;
+    glm::mat4 modelViewProj;
+    int32 frameCount = 0;
+    int32 curNumParticles = 0;
+    std::chrono::time_point<std::chrono::high_resolution_clock> lastFrameTimePoint;
+    static const int32 MaxNumParticles = 1<<16;
+    struct {
+        glm::vec4 pos;
+        glm::vec4 vec;
+    } particles[MaxNumParticles];
+    
+    // shader slots
+    static const int32 ModelViewProjection = 0;
+    static const int32 ParticleTranslate = 1;
+};
+OryolMain(DrawCallPerfApp);
+
+// the vertex shader
+static const char* vsSource =
+"uniform mat4 mvp;\n"
+"uniform vec4 particleTranslate;\n"
+"VS_INPUT(vec4, position);\n"
+"VS_INPUT(vec4, color0);\n"
+"VS_OUTPUT(vec4, color);\n"
+"void main() {\n"
+"  gl_Position = mvp * (position + particleTranslate);\n"
+"  color = color0;\n"
+"}\n";
+
+// the pixel shader
+static const char* fsSource =
+"FS_INPUT(vec4, color);\n"
+"void main() {\n"
+"  FragmentColor = color;\n"
+"}\n";
+
+//------------------------------------------------------------------------------
+AppState::Code
+DrawCallPerfApp::OnInit() {
+    // setup rendering system
+    this->render = RenderFacade::CreateSingle(RenderSetup::Windowed(800, 500, "Oryol DrawCallPerf Sample"));
+    this->render->AttachLoader(RawMeshLoader::Create());
+
+    // create a small cube shape
+    ShapeBuilder shapeBuilder;
+    shapeBuilder.SetRandomColorsFlag(true);
+    shapeBuilder.AddComponent(VertexAttr::Position, VertexFormat::Float3);
+    shapeBuilder.AddComponent(VertexAttr::Color0, VertexFormat::Float4);
+    shapeBuilder.AddBox(0.05f, 0.05f, 0.05f, 1);
+    shapeBuilder.Build();
+    this->meshId = this->render->CreateResource(MeshSetup::FromData("box"), shapeBuilder.GetStream());
+
+    // build a shader program from vs/fs sources
+    ProgramBundleSetup progSetup("prog");
+    progSetup.AddProgramFromSources(0, vsSource, fsSource);
+    progSetup.AddUniform("mvp", ModelViewProjection);
+    progSetup.AddUniform("particleTranslate", ParticleTranslate);
+    this->progId = this->render->CreateResource(progSetup);
+    
+    // setup projection and view matrices
+    const float32 fbWidth = this->render->GetDisplayAttrs().GetFramebufferWidth();
+    const float32 fbHeight = this->render->GetDisplayAttrs().GetFramebufferHeight();
+    this->proj = glm::perspectiveFov(glm::radians(45.0f), fbWidth, fbHeight, 0.01f, 100.0f);
+    this->view = glm::lookAt(glm::vec3(0.0f, 2.5f, 0.0f), glm::vec3(0.0f, 0.0f, -10.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    this->model = glm::mat4();
+    this->modelViewProj = this->proj * this->view * this->model;
+    
+    return App::OnInit();
+}
+
+//------------------------------------------------------------------------------
+void
+DrawCallPerfApp::updateCamera() {
+    float32 angle = this->frameCount * 0.01f;
+    glm::vec3 pos(glm::sin(angle) * 10.0f, 2.5f, glm::cos(angle) * 10.0f);
+    this->view = glm::lookAt(pos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    this->modelViewProj = this->proj * this->view * this->model;
+}
+
+//------------------------------------------------------------------------------
+void
+DrawCallPerfApp::emitParticles() {
+    if (this->curNumParticles < MaxNumParticles) {
+        this->particles[this->curNumParticles].pos = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f);
+        glm::vec3 rnd = glm::ballRand(0.5f);
+        rnd.y += 2.0f;
+        this->particles[this->curNumParticles].vec = glm::vec4(rnd, 0.0f);
+        this->curNumParticles++;
+    }
+}
+
+//------------------------------------------------------------------------------
+void
+DrawCallPerfApp::updateParticles() {
+    const float32 frameTime = 1.0f / 60.0f;
+    for (int32 i = 0; i < this->curNumParticles; i++) {
+        auto& curParticle = this->particles[i];
+        curParticle.vec.y -= 1.0f * frameTime;
+        curParticle.pos += curParticle.vec * frameTime;
+        if (curParticle.pos.y < -2.0f) {
+            curParticle.pos.y = -1.8f;
+            curParticle.vec.y = -curParticle.vec.y;
+            curParticle.vec *= 0.8f;
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+AppState::Code
+DrawCallPerfApp::OnRunning() {
+    
+    // FIXME: that chrono code is ugly, create our own wrapper Time module?
+    
+    using namespace std;
+    
+    chrono::microseconds updTime;
+    chrono::microseconds drawTime;
+    this->frameCount++;
+    if (this->render->BeginFrame()) {
+        
+        // update block
+        auto updStart = chrono::high_resolution_clock::now();
+        this->updateCamera();
+        this->emitParticles();
+        this->updateParticles();
+        updTime = chrono::duration_cast<chrono::microseconds>(chrono::high_resolution_clock::now() - updStart);
+        
+        // render block
+        this->render->ApplyState(Render::State::DepthMask, true);
+        this->render->ApplyState(Render::State::DepthTestEnabled, true);
+        this->render->ApplyState(Render::State::DepthFunc, Render::State::LessEqual);
+        this->render->ApplyState(Render::State::ClearDepth, 1.0f);
+        this->render->ApplyState(Render::State::ClearColor, 0.0f, 0.0f, 0.0f, 0.0f);
+        this->render->Clear(true, true, true);
+        this->render->ApplyProgram(this->progId, 0);
+        this->render->ApplyMesh(this->meshId);
+        this->render->ApplyVariable(ModelViewProjection, this->modelViewProj);
+
+        auto drawStart = chrono::high_resolution_clock::now();
+        for (int32 i = 0; i < this->curNumParticles; i++) {
+            this->render->ApplyVariable(ParticleTranslate, this->particles[i].pos);
+            this->render->Draw(0);
+        }
+        drawTime = chrono::duration_cast<chrono::microseconds>(chrono::high_resolution_clock::now() - drawStart);
+        this->render->EndFrame();
+    }
+    
+    auto curTime = chrono::high_resolution_clock::now();
+    auto frameTime = chrono::duration_cast<chrono::microseconds>(curTime - this->lastFrameTimePoint);
+    this->lastFrameTimePoint = curTime;
+    
+    if (0 == (this->curNumParticles % 200)) {
+        Log::Info("%d draws: upd=%lld draw=%lld, frame=%lld microseconds\n",
+                  this->curNumParticles,
+                  updTime.count(),
+                  drawTime.count(),
+                  frameTime.count());
+    }
+    
+    return render->QuitRequested() ? AppState::Cleanup : AppState::Running;
+}
+
+//------------------------------------------------------------------------------
+AppState::Code
+DrawCallPerfApp::OnCleanup() {
+    // cleanup everything
+    this->render->DiscardResource(this->progId);
+    this->render->DiscardResource(this->meshId);
+    this->render = nullptr;
+    RenderFacade::DestroySingle();
+    return App::OnCleanup();
+}
