d27a9f1b075951e5ea5fe90beabf3eda7fcbe0d8
Andre Weissflog <floooh@gmail.com>
2014-05-30 20:45:12 +0200

    More advanced text rendering sample

--- code/Samples/Debug/DebugText/DebugText.cc
+++ code/Samples/Debug/DebugText/DebugText.cc
@@ -5,6 +5,7 @@
 #include "Core/App.h"
 #include "Render/RenderFacade.h"
 #include "Debug/DebugFacade.h"
+#include "glm/gtc/random.hpp"
 
 using namespace Oryol;
 using namespace Oryol::Core;
@@ -19,30 +20,111 @@ public:
     virtual AppState::Code OnCleanup();
     
 private:
+    void dropChar();
+    void moveChars();
+    void drawText();
+
     RenderFacade* render;
     DebugFacade* debug;
+    int32 width;
+    int32 height;
+    uint8* buffer = nullptr;
+    Core::StringBuilder strBuilder;
 };
 OryolMain(DebugTextApp);
 
 //------------------------------------------------------------------------------
 AppState::Code
 DebugTextApp::OnInit() {
-    this->render = RenderFacade::CreateSingle(RenderSetup::Windowed(1024, 768, "Oryol DebugText Sample"));
+    this->render = RenderFacade::CreateSingle(RenderSetup::Windowed(800, 600, "Oryol DebugText Sample"));
     this->debug  = DebugFacade::CreateSingle();
-    this->debug->SetTextColor(glm::vec4(0.0f, 1.0f, 0.0f, 1.0f));
-    this->debug->SetTextScale(glm::vec2(1.0f, 2.0f));
+    this->debug->SetTextScale(glm::vec2(2.0f, 2.0f));
+    
+    this->width = this->render->GetDisplayAttrs().GetFramebufferWidth() / 16;
+    this->height = this->render->GetDisplayAttrs().GetFramebufferHeight() / 16;
+    this->buffer = (uint8*) Memory::Alloc(this->width * this->height);
+    Memory::Clear(this->buffer, this->width * this->height);
+    
+    this->strBuilder.Reserve(this->width * 2);
     
     return App::OnInit();
 }
 
 //------------------------------------------------------------------------------
+//  Drop a random new character at row 0
+//
+void
+DebugTextApp::dropChar() {
+    int chr = (int) glm::linearRand(33.0f, 127.0f);
+    int col = (int) glm::linearRand(0.0f, float(this->width));
+    this->buffer[col] = chr;
+}
+
+//------------------------------------------------------------------------------
+//  Scan character buffer from bottom to top and move characters toward
+//  bottom which have an empty cell below them.
+//
+void
+DebugTextApp::moveChars() {
+    bool isFull = true;
+    for (int y = this->height - 1; y >= 1; y--) {
+        int yOffset = y * this->width;
+        for (int x = 0; x < this->width; x++) {
+            int offset = yOffset + x;
+            int above  = offset - this->width;
+            if (this->buffer[offset] == 0) {
+                isFull = false;
+                if (this->buffer[above] != 0) {
+                    this->buffer[offset] = this->buffer[above];
+                    this->buffer[above] = 0;
+                }
+            }
+        }
+    }
+    if (isFull) {
+        Memory::Clear(this->buffer, this->width * this->height);
+    }
+}
+
+//------------------------------------------------------------------------------
+//  Draw the character buffer into a string.
+//
+void
+DebugTextApp::drawText() {
+
+    glm::vec4 color[3] = {
+        glm::vec4(1.0f, 0.0f, 0.0f, 1.0f),
+        glm::vec4(0.0f, 1.0f, 0.0f, 1.0f),
+        glm::vec4(0.0f, 0.0f, 1.0f, 1.0f)
+    };
+
+    for (int y = 0; y < this->height; y++) {
+        this->strBuilder.Clear();
+        int yOffset = y * this->width;
+        for (int x = 0; x < this->width; x++) {
+            char c = this->buffer[yOffset + x];
+            if (0 == c) {
+                strBuilder.Append(0x09);    // cursor right
+            }
+            else {
+                strBuilder.Append(c);
+            }
+        }
+        strBuilder.Append("\r\n");
+        this->debug->TextColor(color[y % 3]);
+        this->debug->Print(strBuilder.AsCStr());
+    }
+}
+
+//------------------------------------------------------------------------------
 AppState::Code
 DebugTextApp::OnRunning() {
-    // we can do text rendering everywhere in the frame...
-    this->debug->Text("Hello World!\n\rBlarghl?!?");
-    
     // render one frame
     if (this->render->BeginFrame()) {
+    
+        this->dropChar();
+        this->moveChars();
+        this->drawText();
 
         // clear, apply mesh and shader program, and draw
         this->render->ApplyState(Render::State::ClearColor, 0.5f, 0.5f, 0.5f, 0.0f);
@@ -61,6 +143,8 @@ DebugTextApp::OnRunning() {
 //------------------------------------------------------------------------------
 AppState::Code
 DebugTextApp::OnCleanup() {
+    Memory::Free(this->buffer);
+    this->buffer = nullptr;
     this->render = nullptr;
     this->debug  = nullptr;
     DebugFacade::DestroySingle();
