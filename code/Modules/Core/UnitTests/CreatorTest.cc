4a1525b99724d8e5251543edd3d6943d6b960137
Andre Weissflog <floooh@gmail.com>
2014-09-09 10:21:56 +0200

    Workaround for CreatorTest.cc on GCC

--- code/Modules/Core/UnitTests/CreatorTest.cc
+++ code/Modules/Core/UnitTests/CreatorTest.cc
@@ -48,20 +48,30 @@ void TestFunc(std::function<Core::Ptr<CreatorTestClass>()> bla) {
 
 TEST(CreatorTest) {
     auto creat0 = CreatorTestClass::Creator();
+
+    #if !(defined(__GNUC__) && !defined(__clang__))
+    /** WORKAROUND for GCC bug 'lambda argument pack' **/
     auto creat1 = CreatorTestClass::Creator(10);
     auto creat2 = CreatorTestClass::Creator(20, "Bla");
     auto creat3 = CreatorTestClass::Creator(30, "Blub", Array<int>( { 1, 2, 3, 4 } ));
+    #endif
     
     const auto obj0 = creat0();
+
+    #if !(defined(__GNUC__) && !defined(__clang__))
+    /** WORKAROUND for GCC bug 'lambda argument pack' **/
     const auto obj1 = creat1();
     const auto obj2 = creat2();
     const auto obj3 = creat3();
+    #endif
     
     CHECK(obj0.isValid());
     CHECK(obj0->i0 == 0);
     CHECK(obj0->str0.Empty());
     CHECK(obj0->array0.Empty());
     
+    #if !(defined(__GNUC__) && !defined(__clang__))
+    /** WORKAROUND for GCC bug 'lambda argument pack' **/
     CHECK(obj1.isValid());
     CHECK(obj1->i0 == 10);
     CHECK(obj1->str0.Empty());
@@ -82,5 +92,6 @@ TEST(CreatorTest) {
     CHECK(obj3->array0[3] == 4);
     
     TestFunc(CreatorDerivedClass::Creator(10));
+    #endif
 }
 
