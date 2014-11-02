75a0213e987537c9e62a569d8bb8db41ae7a92d7
Andre Weissflog <floooh@gmail.com>
2014-11-02 22:48:28 +0100

    Removed XML code generation stuff

--- cmake/oryol_generators.cmake
+++ cmake/oryol_generators.cmake
@@ -5,43 +5,6 @@
 #-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
-#   Handle XML generator files before the dependent target exists. This
-#   checks if the target source/header files already exist, if not
-#   dummy files will be generated (the generator script will not be run).
-#
-macro(oryol_handle_xml_files_pretarget xmlFiles)
-    foreach(xmlFile ${xmlFiles})
-        string(REPLACE .xml .cc src ${xmlFile})
-        string(REPLACE .xml .h hdr ${xmlFile})
-        if (NOT EXISTS ${src})
-            file(WRITE ${src} " ")
-        endif()
-        if (NOT EXISTS ${hdr})
-            file(WRITE ${hdr} " ")
-        endif()
-    endforeach()    
-endmacro()
-
-#-------------------------------------------------------------------------------
-#   Handle XML generator files after the target has been created. This
-#   adds a custom target to update the generated C++ sources when the
-#   XML file changes and adds the custom target as dependency to the target
-#
-macro(oryol_handle_xml_files_posttarget target xmlFiles)
-    if (PYTHON)
-        # ...and add a custom target to build the sources
-        add_custom_target(${target}_xml 
-                          COMMAND ${PYTHON} ${ORYOL_ROOT_DIR}/generators/xmlgen.py ${xmlFiles} 
-                          WORKING_DIRECTORY ${ORYOL_ROOT_DIR}/generators
-                          COMMENT "Generating sources...")
-        set_target_properties(${target}_xml PROPERTIES FOLDER "Generators")
-        add_dependencies(${target} ${target}_xml)
-    else()  
-        message("WARNING: Python not found, skipping XML generators!")
-    endif()
-endmacro()
-
-#-------------------------------------------------------------------------------
 #   Adds a .cc and .h file for each .py generator file.
 #
 macro(oryol_handle_py_files_pretarget pyFiles)
