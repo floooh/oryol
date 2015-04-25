361c5c73fc3db85db06d824d56ad64187ad10f9f
Andre Weissflog <floooh@gmail.com>
2015-04-25 19:37:23 +0200

    Added imgui UI to SynthTest sample

--- code/Samples/Synth/SynthTest/SynthTest.cc
+++ code/Samples/Synth/SynthTest/SynthTest.cc
@@ -4,7 +4,7 @@
 #include "Pre.h"
 #include "Core/App.h"
 #include "Gfx/Gfx.h"
-#include "Dbg/Dbg.h"
+#include "IMUI/IMUI.h"
 #include "Input/Input.h"
 #include "Synth/Synth.h"
 
@@ -18,6 +18,8 @@ public:
     
 private:
     int32 frameCount = 0;
+    bool modFreqEnabled = true;
+    float volume = 0.05f;
     static const int NumTracks = 4;
     SynthOp freqOp;     // frequency modulation
     SynthOp sndOp;      // sound waveform
@@ -31,14 +33,21 @@ OryolMain(SynthTestApp);
 AppState::Code
 SynthTestApp::OnInit() {
     Gfx::Setup(GfxSetup::Window(640, 400, "Oryol Synth Test Sample"));
-    Dbg::Setup();
+    IMUI::Setup();
     Input::Setup();
     SynthSetup synthSetup;
     synthSetup.UseGPUSynthesizer = false;
+    synthSetup.InitialVolume = this->volume;
     Synth::Setup(synthSetup);
-    
+
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.WindowRounding = 0.0f;
+    style.Colors[ImGuiCol_TitleBg] = ImVec4(0.5f, 1.0f, 0.5f, 0.7f);
+    style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.5f, 1.0f, 0.5f, 0.20f);
+    style.Colors[ImGuiCol_CheckMark] = ImVec4(0.5f, 0.8f, 0.5f, 0.5f);
+
     this->freqOp.Op = SynthOp::Replace;
-    this->freqOp.Wave = SynthOp::Custom0;
+    this->freqOp.Wave = SynthOp::Sine;
     this->freqOp.Freq = 5;
     this->freqOp.Amp = ToAmp(0.5f);
     this->freqOp.Bias = ToAmp(0.25f);
@@ -50,6 +59,9 @@ SynthTestApp::OnInit() {
     
     Synth::AddOp(0, 0, this->freqOp);
     Synth::AddOp(0, 1, this->sndOp);
+
+    // type compatibility test between ImGui and SynthOp::WaveT
+    o_assert_dbg(sizeof(this->sndOp.Wave) == sizeof(int));
     
     return App::OnInit();
 }
@@ -58,129 +70,46 @@ SynthTestApp::OnInit() {
 AppState::Code
 SynthTestApp::OnRunning() {
 
-    const Keyboard& kbd = Input::Keyboard();
-    if (kbd.KeyDown(Key::Escape)) {
-        if (this->sndOp.Op == SynthOp::ModFreq) {
-            this->sndOp.Op = SynthOp::Replace;
+    Gfx::ApplyDefaultRenderTarget();
+    Gfx::Clear(PixelChannel::All, glm::vec4(0.5f));
+
+    static const char* waveFormNames[SynthOp::NumWaves] = {
+        "Const", "Sine", "SawTooth", "Triangle", "Square", "Noise",
+        "Custom0", "Custom1", "Custom2", "Custom3", "Custom4", "Custom5", "Custom6", "Custom7"
+    };
+
+    IMUI::NewFrame();
+    ImGui::Begin("Synth Test", nullptr, ImVec2(540, 180), 1.0f, 0);
+    if (ImGui::SliderFloat("Volume", &this->volume, 0.001f, 1.0f, "%.5f", 3.0f)) {
+        Synth::UpdateVolume(this->volume);
+    }
+    if (ImGui::Checkbox("Enable Frequency Modulation", &this->modFreqEnabled)) {
+        if (this->modFreqEnabled) {
+            this->sndOp.Op = SynthOp::ModFreq;
         }
         else {
-            this->sndOp.Op = SynthOp::ModFreq;
+            this->sndOp.Op = SynthOp::Replace;
         }
         Synth::AddOp(0, 1, this->sndOp);
     }
-    
-    if (kbd.KeyDown(Key::Up)) {
-        if (kbd.KeyPressed(Key::LeftShift)) {
-            this->freqOp.Freq += 2;
+    if (SynthOp::ModFreq == this->sndOp.Op) {
+        if (ImGui::SliderInt("Modulation Frequency", &this->freqOp.Freq, 1, 60)) {
             Synth::AddOp(0, 0, this->freqOp);
         }
-        else {
-            this->sndOp.Freq += 11;
-            if (this->sndOp.Freq > 4400) {
-                this->sndOp.Freq = 4400;
-            }
-            Synth::AddOp(0, 1, this->sndOp);
-        }
-    }
-    if (kbd.KeyDown(Key::Down)) {
-        if (kbd.KeyPressed(Key::LeftShift)) {
-            this->freqOp.Freq -= 1;
-            if (this->freqOp.Freq < 1) {
-                this->freqOp.Freq = 1;
-            }
+        if (ImGui::Combo("Modulation Waveform", (int*) &this->freqOp.Wave, waveFormNames, SynthOp::NumWaves)) {
             Synth::AddOp(0, 0, this->freqOp);
         }
-        else {
-            this->sndOp.Freq -= 11;
-            if (this->sndOp.Freq < 11) {
-                this->sndOp.Freq = 11;
-            }
-            Synth::AddOp(0, 1, this->sndOp);
-        }
     }
-    
-    for (int i = 0; i < 8; i++) {
-        if (kbd.KeyDown(Key::Code(Key::N1 + i))) {
-            if (kbd.KeyPressed(Key::LeftShift)) {
-                this->freqOp.Wave = SynthOp::WaveT(SynthOp::Custom0 + i);
-                Synth::AddOp(0, 0, this->freqOp);
-            }
-            else {
-                this->sndOp.Wave = SynthOp::WaveT(SynthOp::Custom0 + i);
-                Synth::AddOp(0, 1, this->sndOp);
-            }
-        }
+    if (ImGui::SliderInt("Sound Frequency", &this->sndOp.Freq, 11, 4400)) {
+        Synth::AddOp(0, 1, this->sndOp);
     }
-    
-    // modulation
-    /*
-    if (kbd.KeyDown(Key::N1)) {
-        this->modType = "Silence";
-        SynthOp mod;
-        mod.Code = SynthOp::Const;
-        mod.Amp = 0;
-        Synth::AddOp(0, 1, mod);
-        Synth::AddOp(0, 0, this->op);
+    if (ImGui::Combo("Sound Waveform", (int*) &this->sndOp.Wave, waveFormNames, SynthOp::NumWaves)) {
+        Synth::AddOp(0, 1, this->sndOp);
     }
-    if (kbd.KeyDown(Key::N2)) {
-        this->modType = "Constant One";
-        SynthOp mod;
-        mod.Code = SynthOp::Const;
-        Synth::AddOp(0, 1, mod);
-        Synth::AddOp(0, 0, this->op);
-    }
-    if (kbd.KeyDown(Key::N3)) {
-        this->modType = "LowFreq Sine";
-        SynthOp mod;
-        mod.Code = SynthOp::Sine;
-        mod.Freq = 5;
-        Synth::AddOp(0, 1, mod);
-        Synth::AddOp(0, 0, this->op);
-    }
-    if (kbd.KeyDown(Key::N4)) {
-        this->modType = "HiFreq Sawtooth";
-        SynthOp mod;
-        mod.Code = SynthOp::Triangle;
-        mod.Freq = 55;
-        mod.Amp = 0.5f;
-        mod.Bias = 0.5f;
-        Synth::AddOp(0, 1, mod);
-        Synth::AddOp(0, 0, this->op);
-    }
-    */
-    /*
-    if (kbd.KeyDown(Key::N5)) {
-        // ASDR modulation
-        this->modType = "ADSR";
-        SynthOp attack;
-        attack.Code = SynthOp::Const;
-        attack.FadeIn = 200;
-        attack.Amp = Synth::Amp(1.0f);
-        Synth::AddOp(0, 1, attack);
-        SynthOp decay;
-        decay.Code = SynthOp::Const;
-        decay.FadeIn = 4000;
-        decay.Amp = Synth::Amp(0.2f);
-        Synth::AddOp(0, 1, decay, 0.01f);
-        SynthOp release;
-        release.Code = SynthOp::Const;
-        release.FadeIn = 20000;
-        release.Amp = 0;
-        Synth::AddOp(0, 1, release, 0.3f);
-        Synth::AddOp(0, 0, this->op);
-    }
-    */
+    ImGui::End();
 
     Synth::Update();
-    Dbg::Print("\n\n");
-    Dbg::PrintF(" Frequency Modulation (Esc): %s\n\r", this->sndOp.Op == SynthOp::ModFreq ? "on" : "off");
-    Dbg::PrintF(" Sound Wave (1..8): %s\n\r", SynthOp::ToString(this->sndOp.Wave));
-    Dbg::PrintF(" Sound Freq (up/down): %d\n\r", this->sndOp.Freq);
-    Dbg::PrintF(" Freq Mod Wave (Shift 1..8): %s\n\r", SynthOp::ToString(this->freqOp.Wave));
-    Dbg::PrintF(" Freq Mod Freq (Shift up/down): %d\n\r", this->freqOp.Freq);
-    Gfx::ApplyDefaultRenderTarget();
-    Gfx::Clear(PixelChannel::RGBA, glm::vec4(0.5f), 1.0f, 0);
-    Dbg::DrawTextBuffer();
+    ImGui::Render();
     Gfx::CommitFrame();
     this->frameCount++;
     
@@ -192,8 +121,8 @@ SynthTestApp::OnRunning() {
 AppState::Code
 SynthTestApp::OnCleanup() {
     Synth::Discard();
-    Dbg::Discard();
     Input::Discard();
+    IMUI::Discard();
     Gfx::Discard();
     return App::OnCleanup();
 }
