4afe71de6b94a660e24111b0e3c6b14a4259c594
Andre Weissflog <floooh@gmail.com>
2015-08-16 18:11:47 +0200

    D3D12 preparations

---
+++ code/Modules/Gfx/d3d12/d3d12DisplayMgr.cc
@@ -0,0 +1,14 @@
+//------------------------------------------------------------------------------
+//  d3d12DisplayMgr.cc
+//------------------------------------------------------------------------------
+#include "Pre.h"
+#include "d3d12DisplayMgr.h"
+
+namespace Oryol {
+namespace _priv {
+
+
+
+} // namespace _priv
+} // namespace Oryol
+
