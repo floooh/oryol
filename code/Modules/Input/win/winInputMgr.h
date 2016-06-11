24b9a41fdd4218406dceafbc897d6e0d31e661a0
Andre Weissflog <floooh@gmail.com>
2016-06-11 12:22:18 +0200

    Fixes for Input module changes on Windows with non-GL

--- code/Modules/Input/win/winInputMgr.h
+++ code/Modules/Input/win/winInputMgr.h
@@ -9,7 +9,7 @@
     of the window and input handling code has been copied from GLFW to ensure
     same behaviour between the GL and D3D versions of Oryol.
 */
-#include "Input/base/inputMgrBase.h"
+#include "Input/Core/inputMgrBase.h"
 
 namespace Oryol {
 namespace _priv {
