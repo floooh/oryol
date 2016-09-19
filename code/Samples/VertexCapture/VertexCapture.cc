150180fc8c893cab7b40e9aadaaa5dc9b39818ef
Andre Weissflog <floooh@gmail.com>
2016-09-19 19:26:39 +0200

    More VertexCapture sample stuff

--- code/Samples/VertexCapture/VertexCapture.cc
+++ code/Samples/VertexCapture/VertexCapture.cc
@@ -5,6 +5,9 @@
 #include "Core/Main.h"
 #include "Gfx/Gfx.h"
 #include "glm/gtc/random.hpp"
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "shaders.h"
 
 using namespace Oryol;
 
@@ -17,19 +20,22 @@ public:
     static const int NumPoints = 1024;
     static const int NumPointMeshes = 2;
     StaticArray<Id, NumPointMeshes> pointMeshes;
+    DrawState drawState;
+    Shader::Params vsParams;
 };
 OryolMain(VertexCaptureApp);
 
 //------------------------------------------------------------------------------
 AppState::Code
 VertexCaptureApp::OnInit() {
-    Gfx::Setup(GfxSetup::WindowMSAA4(800, 600, "Oryol Vertex Capture Sample"));
+    auto gfxSetup = GfxSetup::Window(800, 600, "Oryol Vertex Capture Sample");
+    Gfx::Setup(gfxSetup);
 
     if (!Gfx::QueryFeature(GfxFeature::VertexCapture)) {
         o_error("ERROR: VertexCapture not supported!\n");
     }
 
-    // create 2 double-buffered point cloud meshes, start with a random
+    // create 2 double-buffered point cloud meshes, start with a random data set
     auto meshSetup = MeshSetup::FromData();
     meshSetup.Layout.Add(VertexAttr::Position, VertexFormat::Float3);
     meshSetup.NumVertices = NumPoints;
@@ -41,8 +47,23 @@ VertexCaptureApp::OnInit() {
     this->pointMeshes[0] = Gfx::CreateResource(meshSetup, initData, initDataSize);
     Memory::Free(initData);
 
-    // FIXME: pipeline state to render the vertices as point cloud
+    // pipeline state to render the vertices as point cloud
+    Id shd = Gfx::CreateResource(Shader::Setup());
+    auto pipSetup = PipelineSetup::FromLayoutAndShader(meshSetup.Layout, shd);
+    pipSetup.PrimType = PrimitiveType::Points;
+    pipSetup.DepthStencilState.DepthWriteEnabled = true;
+    pipSetup.DepthStencilState.DepthCmpFunc = CompareFunc::Always;
+    pipSetup.RasterizerState.SampleCount = gfxSetup.SampleCount;
+    pipSetup.Layouts[0] = meshSetup.Layout;
+    drawState.Pipeline = Gfx::CreateResource(pipSetup);
+    drawState.Mesh[0] = this->pointMeshes[0];
 
+    const float fbWidth = (const float) Gfx::DisplayAttrs().FramebufferWidth;
+    const float fbHeight = (const float) Gfx::DisplayAttrs().FramebufferHeight;
+    glm::mat4 proj = glm::perspectiveFov(glm::radians(45.0f), fbWidth, fbHeight, 0.01f, 100.0f);
+    glm::mat4 view = glm::mat4();
+    glm::mat4 model = glm::translate(glm::mat4(), glm::vec3(0.0f, 0.0f, -6.0f));
+    this->vsParams.ModelViewProjection = proj * view * model;
 
     return App::OnInit();
 }
@@ -52,6 +73,9 @@ AppState::Code
 VertexCaptureApp::OnRunning() {
 
     Gfx::ApplyDefaultRenderTarget();
+    Gfx::ApplyDrawState(this->drawState);
+    Gfx::ApplyUniformBlock(this->vsParams);
+    Gfx::Draw(PrimitiveGroup(0, NumPoints));
 
     Gfx::CommitFrame();
     return Gfx::QuitRequested() ? AppState::Cleanup : AppState::Running;
