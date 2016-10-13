ffd05380dc3b56efccf9edcc1717da7896316d3e
Andre Weissflog <floooh@gmail.com>
2016-10-13 20:29:03 +0200

    Finalized the MRT sample

--- code/Samples/MultipleRenderTarget/MultipleRenderTarget.cc
+++ code/Samples/MultipleRenderTarget/MultipleRenderTarget.cc
@@ -17,7 +17,7 @@ public:
     AppState::Code OnRunning();
     AppState::Code OnCleanup();
 
-    glm::mat4 computeMVP(const glm::vec3& pos);
+    glm::mat4 computeMVP(float angleX, float angleY, const glm::vec3& pos);
     AppState::Code notSupported();
 
     const int DisplayWidth = 640;
@@ -29,8 +29,13 @@ public:
     DrawState rt0DrawState;
     DrawState rt1DrawState;
     DrawState rt2DrawState;
+
     DrawState cubeDrawState;
-    DisplayShader::VSParams cubeParams;
+    OffscreenShader::VSParams cubeParams;
+
+    DrawState displayDrawState;
+    DisplayShader::VSParams displayParams;
+
     glm::mat4 proj;
     float angleX = 0.0f;
     float angleY = 0.0f;
@@ -40,22 +45,32 @@ OryolMain(MultipleRenderTargetApp);
 //------------------------------------------------------------------------------
 AppState::Code
 MultipleRenderTargetApp::OnInit() {
-    auto gfxSetup = GfxSetup::Window(DisplayWidth, DisplayHeight, "Oryol MRT Sample");
+    auto gfxSetup = GfxSetup::WindowMSAA4(DisplayWidth, DisplayHeight, "Oryol MRT Sample");
     gfxSetup.DefaultClearColor = glm::vec4(0.5f, 0.5f, 0.5f, 1.0f);
     Gfx::Setup(gfxSetup);
     Dbg::Setup();
 
+    // if rendering backend doesn't support MRT, drop out now
+    // and show a warning during frame rendering
     if (!Gfx::QueryFeature(GfxFeature::MultipleRenderTarget)) {
         return App::OnInit();
     }
 
+    // create 3 rendertarget textures, the first one with a DepthStencil
+    // surface, note that the render target textures use MSAA anti-aliasing
     auto rtSetup = TextureSetup::RenderTarget(OffscreenWidth, OffscreenHeight);
     rtSetup.SampleCount = 4;
     rtSetup.DepthFormat = PixelFormat::DEPTHSTENCIL;
+    rtSetup.Sampler.MinFilter = TextureFilterMode::Linear;
+    rtSetup.Sampler.MagFilter = TextureFilterMode::Linear;
     Id rt0 = Gfx::CreateResource(rtSetup);
     rtSetup.DepthFormat = PixelFormat::None;
     Id rt1 = Gfx::CreateResource(rtSetup);
     Id rt2 = Gfx::CreateResource(rtSetup);
+
+    // create a render pass which uses the 3 texture we just created
+    // as color attachments, and the first texture as depth-stencil buffer
+    // FIXME: depth-stencil should be its own texture!
     auto passSetup = RenderPassSetup::From({ rt0, rt1, rt2 }, rt0);
     passSetup.ColorAttachments[0].DefaultClearColor = glm::vec4(0.25f, 0.0f, 0.0f, 1.0f);
     passSetup.ColorAttachments[1].DefaultClearColor = glm::vec4(0.0f, 0.25f, 0.0f, 1.0f);
@@ -63,12 +78,18 @@ MultipleRenderTargetApp::OnInit() {
     passSetup.StoreAction = RenderPassStoreAction::Resolve;
     this->mrtPass = Gfx::CreateResource(passSetup);
 
+    // create a mesh with 2 shapes, a box and a plane
     ShapeBuilder shapeBuilder;
     shapeBuilder.Layout
         .Add(VertexAttr::Position, VertexFormat::Float3)
-        .Add(VertexAttr::Normal, VertexFormat::UByte4N);
+        .Add(VertexAttr::Normal, VertexFormat::UByte4N)
+        .Add(VertexAttr::TexCoord0, VertexFormat::Float2);
     shapeBuilder.Box(1.0f, 1.0f, 1.0f, 1);
-    this->cubeDrawState.Mesh[0] = Gfx::CreateResource(shapeBuilder.Build());
+    shapeBuilder.Plane(1.0f, 1.0f, 1);
+    Id cubeMesh = Gfx::CreateResource(shapeBuilder.Build());
+
+    // create a draw state to render a cube into the
+    // offscreen render targets (this is where the MRT rendering happens)
     Id shd = Gfx::CreateResource(OffscreenShader::Setup());
     auto ps = PipelineSetup::FromLayoutAndShader(shapeBuilder.Layout, shd);
     ps.DepthStencilState.DepthWriteEnabled = true;
@@ -76,7 +97,10 @@ MultipleRenderTargetApp::OnInit() {
     ps.RasterizerState.CullFaceEnabled = true;
     ps.RasterizerState.SampleCount = rtSetup.SampleCount;
     this->cubeDrawState.Pipeline = Gfx::CreateResource(ps);
+    this->cubeDrawState.Mesh[0] = cubeMesh;
 
+    // create a quad-mesh for displaying the 3 render target
+    // textures on screen
     auto quadMeshSetup = MeshSetup::FullScreenQuad();
     Id quadMesh = Gfx::CreateResource(quadMeshSetup);
     Id quadShd = Gfx::CreateResource(QuadShader::Setup());
@@ -85,18 +109,30 @@ MultipleRenderTargetApp::OnInit() {
     ps.DepthStencilState.DepthCmpFunc = CompareFunc::Always;
     ps.RasterizerState.CullFaceEnabled = false;
     ps.RasterizerState.SampleCount = gfxSetup.SampleCount;
-
     this->rt0DrawState.Pipeline = Gfx::CreateResource(ps);
     this->rt0DrawState.Mesh[0] = quadMesh;
-    this->rt0DrawState.FSTexture[Textures::Texture] = rt0;
-
+    this->rt0DrawState.FSTexture[QuadTextures::Texture] = rt0;
     this->rt1DrawState.Pipeline = this->rt0DrawState.Pipeline;
     this->rt1DrawState.Mesh[0] = quadMesh;
-    this->rt1DrawState.FSTexture[Textures::Texture] = rt1;
-
+    this->rt1DrawState.FSTexture[QuadTextures::Texture] = rt1;
     this->rt2DrawState.Pipeline = this->rt0DrawState.Pipeline;
     this->rt2DrawState.Mesh[0] = quadMesh;
-    this->rt2DrawState.FSTexture[Textures::Texture] = rt2;
+    this->rt2DrawState.FSTexture[QuadTextures::Texture] = rt2;
+
+    // and finally create a draw state to render a cube to the
+    // main display which samples the 3 offscreen render targets
+    Id displayShd = Gfx::CreateResource(DisplayShader::Setup());
+    ps = PipelineSetup::FromLayoutAndShader(shapeBuilder.Layout, displayShd);
+    ps.DepthStencilState.DepthWriteEnabled = true;
+    ps.DepthStencilState.DepthCmpFunc = CompareFunc::LessEqual;
+    ps.RasterizerState.CullFaceEnabled = true;
+    ps.RasterizerState.SampleCount = gfxSetup.SampleCount;
+    ps.RasterizerState.CullFaceEnabled = false;
+    this->displayDrawState.Pipeline = Gfx::CreateResource(ps);
+    this->displayDrawState.Mesh[0] = cubeMesh;
+    this->displayDrawState.FSTexture[DisplayTextures::Red] = rt0;
+    this->displayDrawState.FSTexture[DisplayTextures::Green] = rt1;
+    this->displayDrawState.FSTexture[DisplayTextures::Blue] = rt2;
 
     this->proj = glm::perspectiveFov(glm::radians(45.0f), float(OffscreenWidth), float(OffscreenHeight), 0.01f, 100.0f);
 
@@ -107,20 +143,27 @@ MultipleRenderTargetApp::OnInit() {
 AppState::Code
 MultipleRenderTargetApp::OnRunning() {
 
+    // if rendering backend has no MRT support, drop out
     if (!Gfx::QueryFeature(GfxFeature::MultipleRenderTarget)) {
         return this->notSupported();
     }
 
+    // compute all the vertex shader uniforms
     this->angleY += 0.01f;
     this->angleX += 0.02f;
-    this->cubeParams.ModelViewProjection = this->computeMVP(glm::vec3(0, 0, -3));
+    this->cubeParams.ModelViewProjection = this->computeMVP(this->angleX, this->angleY, glm::vec3(0, 0, -3));
+    this->displayParams.ModelViewProjection = this->computeMVP(this->angleX * 0.5f, this->angleY * 0.5f, glm::vec3(0.0f, 0.55f, -3.0f));
+    this->displayParams.Offsets = glm::sin(glm::vec2(this->angleX, this->angleY)) * 0.1f;
 
+    // render the cube into the 3 MRT render targets using a single draw call,
+    // the fragment shader writes 3 colors, one for each color attachment
     Gfx::BeginPass(this->mrtPass);
     Gfx::ApplyDrawState(this->cubeDrawState);
     Gfx::ApplyUniformBlock(this->cubeParams);
-    Gfx::Draw();
+    Gfx::Draw(0);
     Gfx::EndPass();
 
+    // debug-visualize the 3 offscreen render targets at the bottom of the screen
     Gfx::BeginPass();
     Gfx::ApplyViewPort(0, 0, 200, 200);
     Gfx::ApplyDrawState(this->rt0DrawState);
@@ -131,8 +174,15 @@ MultipleRenderTargetApp::OnRunning() {
     Gfx::ApplyViewPort(400, 0, 200, 200);
     Gfx::ApplyDrawState(this->rt2DrawState);
     Gfx::Draw();
-    Gfx::EndPass();
 
+    // render the final plane which samples from all 3 offscreen rendertarget textures
+    const auto& rpAttrs = Gfx::RenderPassAttrs();
+    Gfx::ApplyViewPort(0, 0, rpAttrs.FramebufferWidth, rpAttrs.FramebufferHeight);
+    Gfx::ApplyDrawState(this->displayDrawState);
+    Gfx::ApplyUniformBlock(this->displayParams);
+    Gfx::Draw(1);
+
+    Gfx::EndPass();
     Gfx::CommitFrame();
     return Gfx::QuitRequested() ? AppState::Cleanup : AppState::Running;
 }
@@ -147,10 +197,10 @@ MultipleRenderTargetApp::OnCleanup() {
 
 //------------------------------------------------------------------------------
 glm::mat4
-MultipleRenderTargetApp::computeMVP(const glm::vec3& pos) {
+MultipleRenderTargetApp::computeMVP(float ax, float ay, const glm::vec3& pos) {
     glm::mat4 modelTform = glm::translate(glm::mat4(), pos);
-    modelTform = glm::rotate(modelTform, this->angleX, glm::vec3(1.0f, 0.0f, 0.0f));
-    modelTform = glm::rotate(modelTform, this->angleY, glm::vec3(0.0f, 1.0f, 0.0f));
+    modelTform = glm::rotate(modelTform, ax, glm::vec3(1.0f, 0.0f, 0.0f));
+    modelTform = glm::rotate(modelTform, ay, glm::vec3(0.0f, 1.0f, 0.0f));
     return this->proj * modelTform;
 }
 
