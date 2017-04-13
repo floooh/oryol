d103e9da5dcfba8c85686b51ade0b66d3d0543a2
Andre Weissflog <floooh@gmail.com>
2017-04-13 15:19:17 +0200

    Added a new InlineArray class plus test

---
+++ code/Modules/Core/Containers/InlineArray.h
@@ -0,0 +1,257 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+    @class Oryol::InlineArray
+    @ingroup Core
+    @brief dynamic array without heap allocation and a fixed capacity
+
+    The InlineArray class is a simplified version of Array with
+    a fixed maximum capacity (defined by a template parameter) and
+    no heap allocation.
+
+    NOTE: The capacity is part of the type, thus you can only
+    copy or move from the other InlineArrays with the same
+    capacity!.
+
+    NOTE 2: unused 'invalid' items in the array will be in their
+    default-constructed state
+*/
+#include "Core/Config.h"
+#include "Core/Assertion.h"
+#include <initializer_list>
+
+namespace Oryol {
+
+template<class TYPE, int CAPACITY>  class InlineArray {
+public:
+    /// default constructor
+    InlineArray();
+    /// copy constructor
+    InlineArray(const InlineArray& rhs);
+    /// move constructor (TYPE must be moveable)
+    InlineArray(InlineArray&& rhs);
+    /// setup array from initializer_list
+    InlineArray(std::initializer_list<TYPE> l);
+    /// destructor
+    ~InlineArray();
+
+    /// copy-assignment
+    void operator=(const InlineArray& rhs);
+    /// move-assignment (TYPE must be moveable)
+    void operator=(InlineArray&& rhs);
+
+    /// get number of valid items in the array
+    int Size() const;
+    /// return true if no items in array
+    bool Empty() const;
+    /// get capacity (always identical to CAPACITY templ param)
+    int Capacity() const;
+    /// get number of free slots at back 
+    int Spare() const;
+
+    /// read/write an existing item
+    TYPE& operator[](int index);
+    /// read-only access an existing item
+    const TYPE& operator[](int index) const;
+
+    /// clear the array (destruct items and reset size to 0)
+    void Clear();
+
+    /// copy-add a new item to the back
+    void Add(const TYPE& item);
+    /// move-add a new item to the back
+    void Add(TYPE&& item);
+    /// add new items by initializer_list
+    void Add(std::initializer_list<TYPE> items);
+
+    /// C++ begin
+    TYPE* begin();
+    /// C++ const begin
+    const TYPE* begin() const;
+    /// C++ end
+    TYPE* end();
+    /// C++ const end
+    const TYPE* end() const;
+
+private:
+    /// check if enough room is avaible to add n items, fatal error otherwise
+    void checkRoom(int numItems) const;
+
+    TYPE items[CAPACITY] = { };
+    int size = 0;
+};
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY>
+InlineArray<TYPE, CAPACITY>::InlineArray() {
+    // empty
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY>
+InlineArray<TYPE, CAPACITY>::InlineArray(const InlineArray& rhs) {
+    this->size = rhs.size;
+    for (int i = 0; i < this->size; i++) {
+        this->items[i] = rhs[i];
+    }
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY>
+InlineArray<TYPE, CAPACITY>::InlineArray(InlineArray&& rhs) {
+    this->size = rhs.size;
+    for (int i = 0; i < this->size; i++) {
+        this->items[i] = std::move(rhs[i]);
+    }
+    rhs.size = 0;
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY>
+InlineArray<TYPE, CAPACITY>::InlineArray(std::initializer_list<TYPE> l) {
+    this->size = 0;
+    for (const auto& item : l) {
+        this->Add(item);
+    }
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY>
+InlineArray<TYPE, CAPACITY>::~InlineArray() {
+    this->size = 0;
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> void
+InlineArray<TYPE, CAPACITY>::operator=(const InlineArray& rhs) {
+    if (rhs.size < this->size) {
+        // if rhs has less items, 'destroy' own items that would not be overwritten
+        for (int i = rhs.size; i < this->size; i++) {
+            this->items[i] = TYPE();
+        }
+    }
+    for (int i = 0; i < rhs.size; i++) {
+        this->items[i] = rhs.items[i];
+    }
+    this->size = rhs.size;
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> void
+InlineArray<TYPE, CAPACITY>::operator=(InlineArray&& rhs) {
+    if (rhs.size < this->size) {
+        // if rhs has less items, destroy own items that would not be overwritten
+        for (int i =  rhs.size; i < this->size; i++) {
+            this->items[i] = TYPE();
+        }
+    }
+    for (int i = 0; i < rhs.size; i++) {
+        this->items[i] = std::move(rhs.items[i]);
+    }
+    this->size = rhs.size;
+    rhs.size = 0;
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> int
+InlineArray<TYPE, CAPACITY>::Size() const {
+    return this->size;
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> bool
+InlineArray<TYPE, CAPACITY>::Empty() const {
+    return 0 == this->size;
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> int
+InlineArray<TYPE, CAPACITY>::Capacity() const {
+    return CAPACITY;
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> int
+InlineArray<TYPE, CAPACITY>::Spare() const {
+    return CAPACITY - this->size;
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> TYPE&
+InlineArray<TYPE, CAPACITY>::operator[](int index) {
+    o_assert_range_dbg(index, this->size);
+    return this->items[index];
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> const TYPE&
+InlineArray<TYPE, CAPACITY>::operator[](int index) const {
+    o_assert_range_dbg(index, this->size);
+    return this->items[index];
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> void
+InlineArray<TYPE, CAPACITY>::Clear() {
+    for (int i = 0; i < this->size; i++) {
+        this->items[i] = TYPE();
+    }
+    this->size = 0;
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> void
+InlineArray<TYPE, CAPACITY>::checkRoom(int numItems) const {
+    if ((this->size + numItems) >= CAPACITY) {
+        o_error("No more room in InlineArray!");
+    }
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> void
+InlineArray<TYPE, CAPACITY>::Add(const TYPE& item) {
+    this->checkRoom(1);
+    this->items[this->size++] = item;
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> void
+InlineArray<TYPE, CAPACITY>::Add(TYPE&& item) {
+    this->checkRoom(1);
+    this->items[this->size++] = std::move(item);
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> void
+InlineArray<TYPE, CAPACITY>::Add(std::initializer_list<TYPE> l) {
+    this->checkRoom(l.size());
+    for (const auto& item : l) {
+        this->items[this->size++] = item;
+    }
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> TYPE*
+InlineArray<TYPE, CAPACITY>::begin() {
+    return &(this->items[0]);
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> const TYPE*
+InlineArray<TYPE, CAPACITY>::begin() const {
+    return &(this->items[0]);
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> TYPE*
+InlineArray<TYPE, CAPACITY>::end() {
+    return &(this->items[this->size]);
+}
+
+//------------------------------------------------------------------------------
+template<class TYPE, int CAPACITY> const TYPE*
+InlineArray<TYPE, CAPACITY>::end() const {
+    return &(this->items[this->size]);
+}
+
+} // namespace Oryol
---
+++ code/Modules/Core/UnitTests/InlineArrayTest.cc
@@ -0,0 +1,123 @@
+//------------------------------------------------------------------------------
+//  InlineArrayTest.cc
+//------------------------------------------------------------------------------
+#include "Pre.h"
+#include <algorithm>
+#include "UnitTest++/src/UnitTest++.h"
+#include "Core/Containers/InlineArray.h"
+#include "Core/String/String.h"
+#include "Core/Containers/KeyValuePair.h"
+
+using namespace Oryol;
+
+struct myClass {
+    myClass() { };
+    myClass(int v): val(v) { };
+    ~myClass() { val = 0; };
+    myClass(const myClass& rhs) { val = rhs.val; };
+    myClass(myClass&& rhs) { val = rhs.val; rhs.val = 0; };
+    void operator=(const myClass& rhs) { val = rhs.val; };
+    void operator=(myClass&& rhs) { val = rhs.val; rhs.val = 0; };
+
+    int val = 1;
+};
+
+TEST(InlineArrayTest) {
+
+    InlineArray<myClass, 32> arr0;
+    CHECK(arr0.Empty());
+    CHECK(arr0.Size() == 0);
+    CHECK(arr0.Capacity() == 32);
+    CHECK(arr0.Spare() == 32);
+
+    // populate array 
+    myClass one(1), two(2), three(3);
+    arr0.Add(one);
+    CHECK(!arr0.Empty());
+    CHECK(arr0.Size() == 1);
+    CHECK(arr0.Spare() == 31);
+    CHECK(arr0[0].val == 1);
+    arr0.Add(std::move(two));
+    CHECK(two.val == 0);
+    CHECK(arr0.Size() == 2);
+    CHECK(arr0.Spare() == 30);
+    CHECK(arr0[1].val == 2);
+    two = myClass(2);
+    arr0.Add({ one, two, three });
+    CHECK(arr0.Size() == 5);
+    CHECK(arr0[0].val == 1);
+    CHECK(arr0[1].val == 2);
+    CHECK(arr0[2].val == 1);
+    CHECK(arr0[3].val == 2);
+    CHECK(arr0[4].val == 3);
+
+    // iterate over array
+    int values[] = { 1, 2, 1, 2, 3 };
+    int i = 0;
+    for (auto& v : arr0) {
+        CHECK(v.val == values[i++]);
+    }
+
+    // copy construction
+    InlineArray<myClass, 32> arr1(arr0);
+    CHECK(arr1.Size() == arr0.Size());
+    CHECK(arr1.Capacity() == arr0.Capacity());
+    for (int i = 0; i < arr0.Size(); i++) {
+        CHECK(arr0[i].val == arr1[i].val);
+    }
+
+    // move construction
+    InlineArray<myClass, 32> arr2(std::move(arr0));
+    CHECK(arr2.Size() == 5);
+    CHECK(arr0.Size() == 0);
+    // EVIL CODE to test whether moved elements have been default-initialized,
+    // don't do this in real code
+    CHECK(arr0.begin()->val == 0);
+    // check if movement was successful
+    for (int i = 0; i < arr2.Size(); i++) {
+        CHECK(arr2[i].val == arr1[i].val);
+    }
+
+    // copy-assignment
+    arr0 = { 5, 6, 7, 8, 9, 10 };
+    CHECK(arr0.Size() == 6);
+    arr1 = arr0;
+    CHECK(arr0.Size() == 6);
+    CHECK(arr1.Size() == 6);
+    for (int i = 0; i < 6; i++) {
+        CHECK(arr0[i].val == arr1[i].val);
+    }
+    // move-assignment 
+    arr2 = std::move(arr0);
+    CHECK(arr0.Empty());
+    CHECK(arr2.Size() == 6);
+    for (int i = 0; i < 6; i++) {
+        CHECK(arr2[i].val == arr1[i].val);
+    }
+
+    // check if copying and moving a smaller array into a bigger
+    // array works (the left-over items in target array must 
+    // be default-initialized afterwards)
+    arr0 = { 1, 2, 3 };
+    arr1 = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+    arr2 = arr1;
+    arr1 = arr0;
+    CHECK(arr1[0].val == 1);
+    CHECK(arr1[1].val == 2);
+    CHECK(arr1[2].val == 3);
+    // EVIL, DON'T DO THIS in actual code
+    for (int i = 3; i < 10; i++) {
+        CHECK(arr1.begin()[i].val == 0);
+    }
+
+    arr2 = std::move(arr0);
+    CHECK(arr0.Empty());
+    CHECK(arr2[0].val == 1);
+    CHECK(arr2[1].val == 2);
+    CHECK(arr2[2].val == 3);
+    // EVIL, DON'T DO THIS in actual code
+    for (int i = 3; i < 10; i++) {
+        CHECK(arr2.begin()[i].val == 0);
+    }
+}
+
