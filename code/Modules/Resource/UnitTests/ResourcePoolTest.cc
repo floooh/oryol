bf4a64b78bf455c7c255a34b2eaedc240c5717bb
Andre Weissflog <floooh@gmail.com>
2017-05-23 19:50:31 +0200

    Fixed tests

--- code/Modules/Resource/UnitTests/ResourcePoolTest.cc
+++ code/Modules/Resource/UnitTests/ResourcePoolTest.cc
@@ -8,19 +8,12 @@
 
 using namespace Oryol;
 
-class mySetup {
-public:
-    mySetup() : bla(0) {};
-    mySetup(int bla_) : bla(bla_) {};
-    int bla;
-};
-
-class myResource : public ResourceBase<mySetup> {
+class myResource : public ResourceBase {
 public:
     int blub = 0;
 };
 
-class myResourcePool : public ResourcePool<myResource, mySetup> { };
+class myResourcePool : public ResourcePool<myResource> { };
 
 TEST(ResourcePoolTest) {
     const uint16_t myResourceType = 12;
@@ -38,7 +31,7 @@ TEST(ResourcePoolTest) {
     CHECK(resId.SlotIndex == 0);
     CHECK(ResourceState::InvalidState == resourcePool.QueryState(resId));
     
-    resourcePool.Assign(resId, mySetup(123), ResourceState::Valid);
+    resourcePool.Assign(resId, ResourceState::Valid);
     CHECK(resourcePool.GetNumFreeSlots() == 255);
     CHECK(resourcePool.GetNumUsedSlots() == 1);
     CHECK(ResourceState::Valid == resourcePool.QueryState(resId));
@@ -47,15 +40,14 @@ TEST(ResourcePoolTest) {
     CHECK(nullptr != res);
     CHECK(res->Id == resId);
     CHECK(resourcePool.QueryResourceInfo(resId).State == ResourceState::Valid);
-    CHECK(res->Setup.bla == 123);
-    
+
     Id resId1 = resourcePool.AllocId();
     CHECK(resId1.IsValid());
     CHECK(resId1.Type == 12);
     CHECK(resId1.SlotIndex == 1);
     CHECK(ResourceState::InvalidState == resourcePool.QueryState(resId1));
     
-    resourcePool.Assign(resId1, mySetup(12345), ResourceState::Valid);
+    resourcePool.Assign(resId1, ResourceState::Valid);
     CHECK(resourcePool.GetNumFreeSlots() == 254);
     CHECK(resourcePool.GetNumUsedSlots() == 2);
     CHECK(ResourceState::Valid == resourcePool.QueryState(resId));
@@ -64,8 +56,7 @@ TEST(ResourcePoolTest) {
     CHECK(nullptr != res1);
     CHECK(res1->Id == resId1);
     CHECK(resourcePool.QueryResourceInfo(resId1).State == ResourceState::Valid);
-    CHECK(res1->Setup.bla == 12345);
-    
+
     const ResourcePoolInfo poolInfo = resourcePool.QueryPoolInfo();
     CHECK(poolInfo.ResourceType == myResourceType);
     CHECK(poolInfo.NumSlots == 256);
@@ -76,14 +67,12 @@ TEST(ResourcePoolTest) {
     CHECK(resourcePool.GetNumFreeSlots() == 255);
     CHECK(resourcePool.GetNumUsedSlots() == 1);
     CHECK(resourcePool.QueryState(resId) == ResourceState::InvalidState);
-    CHECK(res->Setup.bla == 123);
-    
+
     resourcePool.Unassign(resId1);
     CHECK(resourcePool.GetNumFreeSlots() == 256);
     CHECK(resourcePool.GetNumUsedSlots() == 0);
     CHECK(resourcePool.QueryState(resId1) == ResourceState::InvalidState);
-    CHECK(res1->Setup.bla == 12345);
-    
+
     resourcePool.Discard();
     CHECK(!resourcePool.IsValid());
 }
